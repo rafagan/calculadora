7976884570648a6581659d7b62e09683cd87d764
Rafagan Abreu <rafagan@guizion.com>
2017-11-16 16:39:06 -0200

    Computed property

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,24 +12,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     var userIsTyping: Bool = false
     
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
     @IBAction func touchClear(_ sender: UIButton) {
         userIsTyping = false
-        display?.text = "0"
+        display.text = "0"
     }
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsTyping {
-            let textDisplay = display?.text ?? ""
-            display?.text = textDisplay + digit
+            let textDisplay = display.text ?? ""
+            display.text = textDisplay + digit
         } else {
-            display?.text = digit
+            display.text = digit
             userIsTyping = true
         }
     }
     
     @IBAction func performOperation(_ sender: UIButton) {
         userIsTyping = false
+
         if let mathSymbol = sender.currentTitle {
             switch mathSymbol {
             case "+":
@@ -39,6 +49,16 @@ class ViewController: UIViewController {
             case "⨉":
                 break;
             case "÷":
+                break;
+            case "=":
+                break;
+            case "±":
+                display.text = String(-displayValue)
+                break;
+            case "%":
+                break;
+            case "√":
+                display.text = String(sqrt(displayValue))
                 break;
             default:
                 break;
