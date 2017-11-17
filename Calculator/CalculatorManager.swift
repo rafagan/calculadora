d2bdef501ddd33e7d2c6b65c3ffc92a8d535465b
Rafagan Abreu <rafagan@guizion.com>
2017-11-17 16:08:38 -0200

    Refactor do switch para considerar dicionário de operações e enum

--- Calculator/CalculatorManager.swift
+++ Calculator/CalculatorManager.swift
@@ -8,8 +8,44 @@
 
 import Foundation
 
+func changeSign(_ value: Double) -> Double {
+    return -value
+}
+
+func plus(_ v1: Double, _ v2: Double) -> Double {
+    return v1 + v2
+}
+
+func minus(_ v1: Double, _ v2: Double) -> Double {
+    return v1 - v2
+}
+
+func times(_ v1: Double, _ v2: Double) -> Double {
+    return v1 * v2
+}
+
+func division(_ v1: Double, _ v2: Double) -> Double {
+    return v1 / v2
+}
+
 struct CalculatorManager {
     private var accumulator: Double?
+    
+    private enum Operation {
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+    }
+    
+    private let operations: Dictionary<String, Operation> = [
+        "√": Operation.unaryOperation(sqrt),
+        "±": Operation.unaryOperation(changeSign),
+        "+": Operation.binaryOperation(plus),
+        "-": Operation.binaryOperation(minus),
+        "⨉": Operation.binaryOperation(times),
+        "÷": Operation.binaryOperation(division),
+        //"=": Operation.binaryOperation(???),
+        //"%": Operation.binaryOperation(???)
+    ]
     
     var result: Double? {
         get {
@@ -18,28 +54,12 @@ struct CalculatorManager {
     }
     
     mutating func performOperation(_ symbol: String) {
-        if let value = accumulator {
-            switch symbol {
-            case "√":
-                accumulator = sqrt(value)
-                break;
-            case "±":
-                accumulator = -value
-                break;
-            case "+":
-                break;
-            case "-":
-                break;
-            case "⨉":
-                break;
-            case "÷":
-                break;
-            case "=":
-                break;
-            case "%":
-                break;
+        if let operation = operations[symbol] {
+            switch operation {
+            case .unaryOperation(let op):
+                accumulator = (accumulator != nil ? op(accumulator!) : accumulator)
             default:
-                break;
+                break
             }
         }
     }
