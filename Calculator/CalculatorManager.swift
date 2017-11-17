8b930c1c0a134174cf8fa31362ffeee496a5395b
Rafagan Abreu <rafagan@guizion.com>
2017-11-17 12:43:02 -0200

    Primeira versão do CalculatorManager

---
+++ Calculator/CalculatorManager.swift
@@ -0,0 +1,27 @@
+//
+//  CalculatorManager.swift
+//  Calculator
+//
+//  Created by Rafagan Abreu on 17/11/17.
+//  Copyright © 2017 CINQ. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorManager {
+    private var accumulator: Double?
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+    func performOperation(_ symbol: String) {
+        
+    }
+    
+    func setOperator(_ operator: Double) {
+        
+    }
+}
